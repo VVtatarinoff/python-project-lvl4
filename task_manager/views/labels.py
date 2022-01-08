6cf7976e3f573cf4c6d6c86bf7085238c36860e6
Vladimir <vvtatarinoff@gmail.com>
2022-01-08 19:59:11 +0300

    deleting label

--- task_manager/views/labels.py
+++ task_manager/views/labels.py
@@ -1,14 +1,15 @@
 import logging
 
 from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.utils.translation import gettext as _
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView, DeleteView
 
 from task_manager.forms.statuses_forms import CreateLabelForm
 from task_manager.views.general import LoginRequiredMessage, SimpleTableView
-from task_manager.models import Label
+from task_manager.models import Label, Task
 from task_manager.views.general import LABEL_CATEGORY
 
 logger = logging.getLogger(__name__)
@@ -34,9 +35,34 @@ class CreateLabel(CreateView):
         return reverse_lazy('labels')
 
 
-def del_label(request):
-    return render(request, "main.html")
+class DeleteLabel(DeleteView):
+    model = Label
+    template_name = 'delete_page.html'
 
+    def get_context_data(self, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Delete label"
+        context['btn_name'] = 'Yes, delete'
+        name = self.get_object().name
+        msg = _('Are you sure you want to delete') + ' ' + name + '?'
+        context['message'] = msg
+        return context
+
+    def get_success_url(self):
+        messages.success(self.request, _('Label was successfully deleted'))
+        return reverse_lazy('labels')
+
+    def form_valid(self, form):
+        object = self.get_object()
+        print(self.object)
+        tasks = Task.objects.filter(labels=object.id).first()
+        print('How many label used:', tasks)
+        if tasks:
+            msg = _('Unable to delete label as it is in use')
+            messages.error(self.request, msg)
+            return HttpResponseRedirect(reverse_lazy('labels'))
+        self.object.delete()
+        return HttpResponseRedirect(self.get_success_url())
 
 class ChangeLabel(UpdateView):
     form_class = CreateLabelForm
